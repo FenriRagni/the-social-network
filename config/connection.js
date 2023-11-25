a54f048b1cb59b31b34fbba08785825fca8d8c39
FenriRagni <145153161+FenriRagni@users.noreply.github.com>
2023-11-24 21:32:19 -0500

    updated DB name

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,7 @@
 const { connect, connection } = require('mongoose');
 
 const connectionString =
-  proces.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/studentsDB';
+  proces.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/socialDB';
 
 connect(connectionString);
 
