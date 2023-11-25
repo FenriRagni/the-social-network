ef01704dbf032595ba76071d70aabfa8eb7887ab
FenriRagni <145153161+FenriRagni@users.noreply.github.com>
2023-11-25 00:31:10 -0500

    created Thought model

--- models/Thought.js
+++ models/Thought.js
@@ -1,2 +1,64 @@
 const { Schema, model } = require('mongoose');
 
+const reactionSchema = new Schema(
+  {
+    reactionId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId(),
+    },
+    reactionBody: {
+      type: String,
+      required: true,
+      maxLength: 280
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now(),
+      get: () => {
+        let date = this.createdAt;
+        return `Created: ${date.getMonth()}/${date.getDate()}/${date.getFullYear()}`;
+      }
+    }
+  }
+)
+
+const thoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      minLength: 1,
+      maxLength: 280
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now(),
+      get: () => {
+        let date = this.createdAt;
+        return `Created: ${date.getMonth()}/${date.getDate()}/${date.getFullYear()}`;
+      }
+    },
+    username: {
+      type: String,
+      required: true
+    },
+    reactions: [reactionSchema]
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    }
+  }
+);
+
+thoughtSchema.virtual('reactionCount').get(function () {
+  return `Reactions: ${this.reactions.length}`;
+})
+
+const Thought = model('thought', thoughtSchema);
+
+module.exports = Thought;
